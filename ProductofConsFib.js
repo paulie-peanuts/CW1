7232c68ca78a9a36a641bfe4c812bf0ed9d01cb3
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-06 09:29:45 -0500

    prod Fib

---
+++ ProductofConsFib.js
@@ -0,0 +1,71 @@
+function productFib(prod){
+    let array = fib(prod);
+    let i=0;
+      while (array[i]*array[i+1] <= prod) {
+        i++;
+      }
+    if (array[i-1]*array[i] === prod) {
+      return [array[i-1], array[i], true];
+    } else {
+     return [array[i], array[i+1], false];
+    }
+  }
+  
+  function fib(end) {
+    let fibs = [0,1];
+    let i=0;
+    let last = 0;
+    while (last <= end) {
+      last = fibs[i]+fibs[i+1];
+      fibs.push(last);
+      i++;
+    }
+    return fibs;
+  }
+  /*
+DESCRIPTION:
+
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true], 
+productFib(800) # should return [34, 55, false], 
+-----
+productFib(714) # should return {21, 34, 1}, 
+productFib(800) # should return {34, 55, 0},        
+-----
+productFib(714) # should return {21, 34, true}, 
+productFib(800) # should return {34, 55, false}, 
+Note:
+
+You can see examples for your language in "Sample Tests".
+  */
