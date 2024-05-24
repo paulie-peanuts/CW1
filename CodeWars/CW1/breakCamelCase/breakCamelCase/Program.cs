adadb588895e3d7bbddfce4e4899cedc6f31438f
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-23 23:11:48 -0500

    breakCamelCase

---
+++ CodeWars/CW1/breakCamelCase/breakCamelCase/Program.cs
@@ -0,0 +1,35 @@
+﻿// using NUnit.Framework;
+// using System;
+// using System.Collections.Generic;
+using System.Text;
+
+public class Kata
+{
+  public static string BreakCamelCase(string str)
+  {
+    string caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char[] letters = str.ToCharArray();
+    StringBuilder answer = new();
+        for (int i=0; i < letters.Length; i++)
+        {
+            if (!caps.Contains(letters[i]))
+            {
+                answer.Append(letters[i]);
+            } else
+            {
+                answer.Append(' ');
+                answer.Append(letters[i]);
+            }
+        }
+        return answer.ToString();
+  }
+}
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
