e9e4607a0b42e7887af94f8d38abd000626ced2f
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-24 21:44:47 -0500

    aretheysame

---
+++ AreTheyTheSame/AreTheyTheSame/AreTheyTheSame.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Xml.XPath;
+namespace AreTheyTheSame;
+class AreTheySame
+{
+  public static bool comp(int[] a, int[] b)
+  {
+    if (a.Length == 0 || b.Length == 0)
+    {
+        return false;
+    }
+    Array.Sort(a);
+    Array.Sort(b);
+    for (int i=0; i<a.Length; i++)
+    {
+        a[i] = a[i] * a[i];
+    }
+    for (int i=0; i<a.Length; i++)
+    {
+        if (a[i] != b[i])
+        {
+            return false;
+        }
+    }
+    return true;
+  }
+}
---
+++ AreTheyTheSame/AreTheyTheSame/Program.cs
@@ -0,0 +1,2 @@
+﻿using AreTheyTheSame;
+Console.WriteLine(AreTheySame.comp([6, 4],[16, 36]));
