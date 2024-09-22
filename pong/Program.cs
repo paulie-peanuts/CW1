c51ead2a8bedbd92e15aad5ac8e3315a6dd3e8bc
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-09-21 22:09:27 -0500

    created the field

--- pong/Program.cs
+++ pong/Program.cs
@@ -8,7 +8,32 @@ namespace Pong
     {
         public static void Main(string[] args)
         {
-            
+            const int fieldLength = 50, fieldWidth = 15;
+            const char fieldTile = '#';
+
+            string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+
+            const int racketLength = fieldWidth / 4;
+            const char racketTile = '|';
+
+            int leftRacketHeight = 0;
+            int rightRacketHeight = 0;
+
+            while(true)
+            {
+                Console.SetCursorPosition(0,0);
+                Console.WriteLine(line);
+
+                Console.SetCursorPosition(0,fieldWidth);
+                Console.WriteLine(line);
+                for(int i = 0; i < racketLength; i++)
+                {
+                    Console.SetCursorPosition(0, i + 1 + leftRacketHeight);
+                    Console.WriteLine(racketTile);
+                    Console.SetCursorPosition(fieldLength - 1, i + 1 + rightRacketHeight);
+                    Console.WriteLine(racketTile);
+                }
+            }
         }
     }
 }
